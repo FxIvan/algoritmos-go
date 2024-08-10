3af6e95a81cef9e0875cd7955f24f8af19cb0472
FxIvan <almendraivan210814@gmail.com>
2024-08-10 20:44:47 -0300

    agarrando las mejores rutas

--- backtracking/main.go
+++ backtracking/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // Función principal para descomponer un número n
@@ -33,7 +34,58 @@ func helper(n int, remain int) []int {
 	return nil
 }
 
+// ////////////////////////////////
+var distances = map[string]map[string]int{
+	"A": {"B": 10, "C": 15, "D": 20},
+	"B": {"A": 10, "C": 35, "D": 25},
+	"C": {"A": 15, "B": 35, "D": 30},
+	"D": {"A": 20, "B": 25, "C": 30},
+}
+
+func getDistance(a, b string) int {
+	return distances[a][b]
+}
+
+func calculateDistance(path []string) int {
+	var distance int
+	for i := 0; i < len(path)-1; i++ {
+		distance = getDistance(path[i], path[i+1])
+	}
+	return distance
+}
+
+func findOptimalRoute(destinations []string) ([]string, int) {
+	var bestPath []string
+	bestDistance := math.MaxInt32
+
+	var backtrack func(currentPath []string, remainingDestinations []string)
+	backtrack = func(currentPath []string, remainingDestinations []string) {
+
+		if len(remainingDestinations) == 0 {
+			currentDistance := calculateDistance(currentPath)
+			if currentDistance < bestDistance {
+				bestDistance = currentDistance
+				bestPath = append([]string{}, currentPath...)
+			}
+			return
+		}
+
+		for i := 0; i < len(remainingDestinations); i++ {
+			nextDestination := remainingDestinations[i]
+			newPath := append(currentPath, nextDestination)
+			newRemainingDestinations := append([]string{}, remainingDestinations[:i]...)
+			newRemainingDestinations = append(newRemainingDestinations, remainingDestinations[i+1:]...)
+			backtrack(newPath, newRemainingDestinations)
+		}
+	}
+
+	backtrack([]string{}, destinations)
+	return bestPath, bestDistance
+}
+
 // Función principal para pruebas
 func main() {
-	fmt.Println(decompose(11)) // [1, 2, 3, 6]
+	//fmt.Println(decompose(11)) // [1, 2, 3, 6]
+	destinations := []string{"A", "B", "C", "D"}
+	fmt.Println(findOptimalRoute(destinations))
 }
